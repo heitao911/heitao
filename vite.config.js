44bcdf5efc22917a32802e2cbb2b2122ae496eac
heitao911 <lidany6686@gmail.com>
2023-10-05 16:17:43 +0800

    打包到docs文件夹

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { fileURLToPath, URL } from "node:url"
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'dist'
+    outDir: 'docs'
   },
   resolve: {
     alias: {
