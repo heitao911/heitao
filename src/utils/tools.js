b2c651a498a1028d4b63dd5b0cc15d836548f89a
heitao911 <lidany6686@gmail.com>
2023-10-09 23:30:29 +0800

    ip 请求修改

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -2,18 +2,19 @@ import http from '@/api/http'
 
 export const getIpCountry = async () => {
     return new Promise((resolve, rejects) => {
-        http.get('http://api.ipify.org/').then(res => {
-            if (res) {
-                return res
-            }
-        }).then((ip) => {
+        http.get('http://api.ipify.org/').then(ip => {
             console.log(ip)
-            http.get(`http://ip-api.com/json/${ip}?lang=zh-CN`).then(res => {
-                console.log(res)
-                if (res.status === 'success') {
-                    resolve(res.countryCode)
-                }
-            })
+            if (ip) {
+                http.get(`http://ip-api.com/json/${ip}?lang=zh-CN`).then(res => {
+                    console.log(res)
+                    if (res.status === 'success') {
+                        resolve(res.countryCode)
+                    }
+                }).catch((err) => {
+                    console.log(err)
+                    rejects(new Error(err))
+                })
+            }
         }).catch((err) => {
             console.log(err)
             rejects(new Error(err))
