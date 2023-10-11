616b11047e28d0438fbcf991df752351075f2d30
fuyz <953958160@qq.com>
2023-10-11 10:42:35 +0800

    eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,26 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
   extends: [
-    'plugin:vue/essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier'
+    // 'plugin:vue/essential',
     // '@vue/standard'
+    'plugin:vue/vue3-essential',
+    'standard'
   ],
   rules: {
     semi: ['error', 'never'],
     'no-unused-vars': 'off',
-    //关闭组件命名规则
+    // 关闭组件命名规则
     'vue/multi-word-component-names': 'off',
     'vue/no-reserved-component-names': 'off',
     'prettier/prettier': 'off',
     'no-debugger': 'off',
     'no-console': 'off',
+    camelcase: 'off'
   }
 }
